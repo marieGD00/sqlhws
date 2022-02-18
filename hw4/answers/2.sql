7446765c3542743bf5def1944711feab1d8208da
marieGD00 <43972725+marieGD00@users.noreply.github.com>
2022-02-18 17:17:13 -0500

    Update 2.sql

---
+++ hw4/answers/2.sql
@@ -0,0 +1,1 @@
+With table_one as (select users.name,users.user_id,count(users.user_id) as review_c from users join review on review.user_id  = users.user_id group by users.user_id order by review_c desc, name limit 10) Select avg(stars) as avg from review join table_one on table_one.user_id = review.user_id;
